64555ee743580f7bb637bbae92a3acfdb6a90d6e
lemuellin <lemuellin@gmail.com>
2024-01-27 11:08:34 -0800

    fix R command

--- README.md
+++ README.md
@@ -1,10 +1,11 @@
 # DXF Generator
+- infinite decimal point: parseFloat...
+
 
 1. Future Development:
     - Read Multiple diameters
     - Leading/Trailing Zeros
-    - ZC and G93s
-    - 
+    - ZC
 
 2. File Input Requirement:
     - End the program with M30
@@ -13,3 +14,4 @@
 3. Feature:
     - Read R commmand (without M01, M02)
     - Read Multiple M25s
+    - Read G93s
--- src/helpers/preProcess.js
+++ src/helpers/preProcess.js
@@ -3,13 +3,33 @@ import groupG93 from "./groupG93";
 
 const preProcess = (array) => {
     // Remove '\r' from each element and filter out empty elements
-    let modifiedArray = array.filter(element => element !== '\r').map(element => element.replace('\r', ''));
+    let modifiedArray1 = array.filter(element => element !== '\r').map(element => element.replace('\r', ''));
     
+    // Add M25s after M08
+    const _modifiedArray2 = modifiedArray1.map((element) => {
+      if (element === 'M08') {
+        return [element, 'M25'];
+      } else {
+        return element;
+      }
+    });
+    // Flatten the Array to get a single-dimensional array
+    const modifiedArray2 = _modifiedArray2.flat();
+
+    // Add M25s 2 elements before R1X123
+    for (let i = 0; i < modifiedArray2.length; i++) {
+      if (modifiedArray2[i].startsWith('R') && !modifiedArray2[i].includes('M02')) {
+        modifiedArray2.splice(i-1, 0, 'M25');
+        i++; // Increment i to skip the next element, as we have just added 'M25' before the current element
+      }
+    }
+
+    // Separate the Program into segments using M25
     let result = [];
     let currentContent = [];
     let startPoint = false;
 
-    for (const line of modifiedArray) {
+    for (const line of modifiedArray2) {
       if(line.startsWith('G93')){
         result.push(currentContent);
         startPoint = false;
@@ -27,6 +47,7 @@ const preProcess = (array) => {
       }
     }
     result = removeEmptyElements(result);
+    console.log(result);
     result = groupG93(result);
 
     return result;
